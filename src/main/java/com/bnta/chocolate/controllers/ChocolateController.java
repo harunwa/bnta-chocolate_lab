b2502624055e0282fa7cdf646f85b8acaa0418b7
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-02-28 15:57:59 +0000

    added controllers

---
+++ src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
@@ -0,0 +1,32 @@
+package com.bnta.chocolate.controllers;
+
+import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.services.ChocolateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/chocolate")
+public class ChocolateController {
+
+    @Autowired
+    ChocolateService chocolateService;
+
+    @PostMapping
+    public ResponseEntity<Chocolate> NewChocolate(@RequestBody Chocolate chocolate){
+        Chocolate savedChocolate = chocolateService.saveChocolate(chocolate);
+        return new ResponseEntity<>(savedChocolate, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Chocolate>> getAllChocolate(){
+        List<Chocolate> chocolates = chocolateService.getAllChocolate();
+        return new ResponseEntity<>(chocolates, HttpStatus.OK);
+    }
+
+
+}
---
+++ src/main/java/com/bnta/chocolate/controllers/EstateController.java
@@ -0,0 +1,31 @@
+package com.bnta.chocolate.controllers;
+
+import com.bnta.chocolate.models.Estate;
+import com.bnta.chocolate.services.EstateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/estate")
+public class EstateController {
+
+    @Autowired
+    EstateService estateService;
+
+    @PostMapping
+    public ResponseEntity<Estate> NewEstate(@RequestBody Estate estate){
+        Estate savedEstate = estateService.saveEstate(estate);
+        return new ResponseEntity<>(savedEstate, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Estate>> getAllEstates(){
+        List<Estate> estates = estateService.getAllEstates();
+        return new ResponseEntity<>(estates, HttpStatus.OK);
+    }
+
+}
