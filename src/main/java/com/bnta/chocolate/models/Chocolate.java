4e58b43eba3a61ac0ac38c71ea0d84ea45afa976
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-02-28 15:15:54 +0000

    initial commit

---
+++ src/main/java/com/bnta/chocolate/models/Chocolate.java
@@ -0,0 +1,68 @@
+package com.bnta.chocolate.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "chocolate")
+public class Chocolate {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "cocoaPercentage")
+    private int cocoaPercentage;
+
+    @ManyToOne
+    @JoinColumn(name = "estate_id")
+    @JsonIgnoreProperties({"chocolate"})
+    @Column(name = "estate")
+    private Estate estate;
+
+    public Chocolate(String name, int cocoaPercentage, Estate estate) {
+        this.name = name;
+        this.cocoaPercentage = cocoaPercentage;
+        this.estate = estate;
+    }
+
+    public Chocolate() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCocoaPercentage() {
+        return cocoaPercentage;
+    }
+
+    public void setCocoaPercentage(int cocoaPercentage) {
+        this.cocoaPercentage = cocoaPercentage;
+    }
+
+    public Estate getEstate() {
+        return estate;
+    }
+
+    public void setEstate(Estate estate) {
+        this.estate = estate;
+    }
+}
